19d170906f42db7741e231e242b65e7ef0a227e7
uzunsahin <sahin.uzun@gratas.com.tr>
2023-04-04 20:49:06 +0300

    Fanout Exchange Type Eklendi

---
+++ 02-FanoutExchange-Consumer/Program.cs
@@ -0,0 +1,40 @@
+﻿//Consumer Console App
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+ConnectionFactory factory = new();
+factory.Uri = new("amqps://agynxcdk:czCiBq-QnX_MgyDat8Iqxl2bVtBOGZFH@woodpecker.rmq.cloudamqp.com/agynxcdk");
+
+
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+
+channel.ExchangeDeclare(exchange: "fanout-exchange-example", ExchangeType.Fanout);
+
+Console.WriteLine("Kuyruk Adını Giriniz");
+string queueName = Console.ReadLine();
+
+channel.QueueDeclare(queue: queueName, exclusive: false);
+
+//Bind mekanizması exchange ile kuyruk arasındaki ilişkilendirilmesi birbirlerine bağlanması işlemi
+channel.QueueBind(
+    queue:queueName,
+    exchange: "fanout-exchange-example",
+    routingKey:string.Empty
+    );
+
+EventingBasicConsumer consumer = new(channel);
+
+channel.BasicConsume(
+    queue: queueName,
+    autoAck:true,
+    consumer:consumer);
+
+consumer.Received += (sender, e) =>
+{
+    string message = Encoding.UTF8.GetString(e.Body.Span);
+    Console.WriteLine(message);
+};
+
+Console.Read();
---
+++ 02-FanoutExchange-Publisher/Program.cs
@@ -0,0 +1,25 @@
+﻿//Consumer Console App
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+ConnectionFactory factory = new();
+factory.Uri = new("amqps://agynxcdk:czCiBq-QnX_MgyDat8Iqxl2bVtBOGZFH@woodpecker.rmq.cloudamqp.com/agynxcdk");
+
+
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+
+channel.ExchangeDeclare("fanout-exchange-example", ExchangeType.Fanout);
+
+for (int i = 0; i < 100; i++)
+{
+    await Task.Delay(200);
+    byte[] message = Encoding.UTF8.GetBytes($"Merhaba {i}");
+    channel.BasicPublish(
+        exchange: "fanout-exchange-example", 
+        routingKey: string.Empty, 
+        body: message);
+}
+
+Console.Read();
